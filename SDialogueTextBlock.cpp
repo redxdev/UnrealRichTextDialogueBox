9a5b0e36a7660c13d58a12831df2ef208ba6146b
Adam Parkinson <salamiarmi@gmail.com>
2023-07-28 11:32:29 +0100

    - (Minor) Cache SDialogueTextBlock desired size.

--- SDialogueTextBlock.cpp
+++ SDialogueTextBlock.cpp
@@ -7,16 +7,20 @@ TAttribute<FText> SDialogueTextBlock::MakeTextAttribute(const FText& typedText, 
 
 FVector2D SDialogueTextBlock::ComputeDesiredSize(float LayoutScaleMultiplier) const
 {
+	return m_cachedDesiredSize;
+}
+
+void SDialogueTextBlock::CacheDesiredSize(float LayoutScaleMultiplier)
+{
 	// calculate actual maxmimum dialogue size
 	isComputingDesiredSize = true;
-	auto result = SRichTextBlock::ComputeDesiredSize(LayoutScaleMultiplier);
+	m_cachedDesiredSize = SRichTextBlock::ComputeDesiredSize(LayoutScaleMultiplier);
 	isComputingDesiredSize = false;
 
 	// poke the method again because this internally caches some junk pertaining to layout/content
 	(void)SRichTextBlock::ComputeDesiredSize(LayoutScaleMultiplier);
 
-	// return the overridden value
-	return result;
+	SRichTextBlock::CacheDesiredSize(LayoutScaleMultiplier);
 }
 
 FText SDialogueTextBlock::GetTextInternal(FText typedText, FText finalText) const
